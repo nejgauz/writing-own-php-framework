e7db1dfd90aa0600d33d0084827ead50c1eb97a0
Анна <shesterikofff@mail.ru>
2020-05-11 14:58:30 +0300

    Убрала prepare

--- resp301.php
+++ resp301.php
@@ -1,7 +1,6 @@
 <?php
 require_once('vendor/autoload.php');
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Request;
 $content = '<h1>Error 301</h1> <h2>The resource moved permanently. Please <a href="\resp200.php">click here</a> to redirect.</h2>';
 $response = new Response(
     $content,
@@ -10,5 +9,4 @@ $response = new Response(
         'cache-control' => 'public',
     ]
 );
-$response->prepare(Request::createFromGlobals());
 $response->send();
