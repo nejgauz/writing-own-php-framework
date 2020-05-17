968ed10bb2623238d0b1cb65b1ff14278a7b7b17
Анна <shesterikofff@mail.ru>
2020-05-17 17:30:12 +0300

    Добавила проверку метода запроса и логику проверки контроллера

--- app/Route.php
+++ app/Route.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 namespace MyFramework;
 
 
+use Symfony\Component\HttpFoundation\Request;
+
 class Route implements RouteInterface
 {
     /**
@@ -17,14 +19,22 @@ class Route implements RouteInterface
     protected $controller;
 
     /**
+     * @var string
+     */
+    protected $method;
+
+
+    /**
      * Route constructor.
      * @param string $url
      * @param ControllerInterface $controller
+     * @param string $method
      */
-    public function __construct(string $url, ControllerInterface $controller)
+    public function __construct(string $url, ControllerInterface $controller, string $method)
     {
         $this->url = $url;
         $this->controller = $controller;
+        $this->method = $method;
     }
 
     /**
@@ -37,16 +47,18 @@ class Route implements RouteInterface
     }
 
     /**
-     * Проверяет, совпадает ли урл с урлом объекта класса
-     * @param string $url
+     * @param Request $request
      * @return bool
      */
-    public function isUrlAcceptable(string $url): bool
+    public function isRequestAcceptable(Request $request): bool
     {
-        if ($url === $this->url) {
-            return true;
+        if (!($request->server->get('REQUEST_URI') === $this->url)) {
+            return false;
         }
-        return false;
+        if (!($request->isMethod($this->method))) {
+            return false;
+        }
+        return true;
     }
 
 }
